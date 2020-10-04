5dd442d66a63c48aaf33eb57ff4040f325891c47
Nova Eeken <novaeeken@Novas-MacBook-Pro.local>
2020-10-04 22:39:33 +0200

    Create answers to exercises

--- objects.js
+++ objects.js
@@ -12,16 +12,20 @@ const product = {
 
 // 1a. Log de naam van de serie in de console.
 
+console.log(product.series);
 
 // 1b. Verander de kleur van het product in "Roze" en log het object in de console
 
+product.color = "roze";
+console.log(product);
 
 // 1c. Log de tweede materiaalsoort ("kunststof") in de console
 
+console.log(product.material[1]);
 
 // 1d. Log de afmetingen (b-h-d) in de console
 
-
+console.log(product["b-h-d"]);
 
 /* Opdracht 2 */
 // Schrijf een losstaande functie die bovenstaand object verwacht. De functie gebruikt de naam van het product
@@ -29,14 +33,25 @@ const product = {
 // ---- Verwachte uitkomst:
 // "De Smeg Waterkoker is gemaakt van roestvrijstaal en kunststof"
 
+function getProductDescription(productObject) {
+    return "De " + productObject.productName + " is gemaakt van " + productObject.material[0] + " en " + productObject.material[1];
+}
 
+console.log(getProductDescription(product));
 
 /* Opdracht 3 */
 // Maak een object met daarin jouw voornaam (string), achternaam (string),
 // leeftijd (getal) en geboortedatum (date object). De datum hoeft niet geformat te worden.
 // Log dit in de console.
 
+const myself = {
+    firstName: "Nova",
+    lastName: "Eeken",
+    age: 27,
+    dateOfBirth: new Date(1992, 11, 4),
+};
 
+console.log(myself);
 
 /* Opdracht */
 // Schrijf een losstaande functie die bovenstaand object verwacht.
@@ -44,3 +59,9 @@ const product = {
 // ---- Verwachte uitkomsten:
 // { age: 27 } geeft "Jij bent 27 jaar oud."
 // { age: 56 } geeft "Jij bent 56 jaar oud."
+
+function yourAge(person) {
+    return "Jij bent " + person.age +  " jaar oud";
+}
+
+console.log(yourAge(myself));
